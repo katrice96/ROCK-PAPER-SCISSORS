e1cfb6305aaace66ebad666cd6afe69faef776e7
colin rice <100053207+katrice96@users.noreply.github.com>
2023-06-23 15:44:29 -0400

    added randomIndex to choose from a random string
     moved Choices array outside of function scope
    
     added const computerChoice to call on getComputer choice and return either rock paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 function getComputerChoice(strings) {
-    const choices = ['rock','paper','scissors']
-    return strings[choices]
+    const randomIndex = Math.floor(Math.random() * strings.length);
+    return strings[randomIndex]
 }
+
+const choices = ['rock','paper','scissors'];
+const computerChoice = getComputerChoice(choices);
+console.log(computerChoice)
