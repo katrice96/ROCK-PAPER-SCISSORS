b1f3431ef544706ae9e5b45704f3520c325c15f1
colin rice <100053207+katrice96@users.noreply.github.com>
2023-07-10 22:28:24 -0400

    added game function which calls playRound 5 times/ and declares a winner based on outcome of rounds added computerCounter and playerCounter to count score added gameCounter function to define how and when previously defined counters iterate
    // known issues //
    player is only able to input one time ( when page loads )

--- javascript.js
+++ javascript.js
@@ -5,9 +5,9 @@ function getComputerChoice(strings) {
     return strings[randomIndex]
 }
 // the array of choices the computer pulls from in this case
-const choices = ['rock','paper','scissors'];
+let choices = ['rock','paper','scissors'];
 // defines the computerChoice variable that tells the getComputerChoice function to pull from the choices array 
-const computerChoice = getComputerChoice(choices);
+let computerChoice = getComputerChoice(choices);
 //logs result of computerChoice variable in console 
 console.log(computerChoice)
 
@@ -43,13 +43,104 @@ function playRound(computerChoice, playerSelection) {
         return 'You lose bozo';
         // defines that every input no previously defined above will return 
     } else { return 'pls input a valid answer' }
+
+    
    
-
-
-
 }
     
+// defines both counters 
+// defined outside of a function to allow to iterate each time gameCounter function is called 
 
+ let playerCounter = 0;
+ let computerCounter = 0;
+    // defines gameCounter function which defines behavior of counters when different conditions are true
+ function gameCounter(computerChoice, playerSelection) {
 
+    if (playerSelection === computerChoice) {
+ 
+        return playerCounter && computerCounter; 
+    
+        
+       } else if ( 
+        (playerSelection === 'rock' && computerChoice === 'scissors') || 
+        (playerSelection === 'paper' && computerChoice === 'rock') || 
+        (playerSelection ==='scissors' && computerChoice === 'paper')
+    
+       ) {
+        playerCounter += 1; 
+        
+       
+       } else if ( 
+        (playerSelection ==='scissors' && computerChoice === 'rock') || 
+        (playerSelection === 'rock' && computerChoice === 'paper') || 
+        (playerSelection === 'paper' && computerChoice === 'scissors')
+        ) {
+            computerCounter += 1 ;
+        }
+        
+        return;
+    
+ }
+
+ // defines the game function which takes in playround function as a parameter to play a five round game of rock paper scissors 
+ 
+ function game(playRound) {
+ // defines computerChoice parameter 
+    computerChoice = getComputerChoice(choices);
+  // this function plays round of rock paper scissors 
+    playRound(computerChoice, playerSelection);
+    // counts who wins, loses or ties in each round( will iterate )
+    gameCounter(computerChoice, playerSelection);
+    // logs output of round
+    console.log(playRound(computerChoice, playerSelection))
+
+    computerChoice = getComputerChoice(choices);
+
+    playRound(computerChoice, playerSelection);
+    gameCounter(computerChoice, playerSelection);
+    console.log(playRound(computerChoice, playerSelection))
+
+    computerChoice = getComputerChoice(choices);
+    
+    playRound(computerChoice, playerSelection);
+    gameCounter(computerChoice, playerSelection);
+    console.log(playRound(computerChoice, playerSelection))
+
+    computerChoice = getComputerChoice(choices);
+
+    playRound(computerChoice, playerSelection);
+    gameCounter(computerChoice, playerSelection);
+    console.log(playRound(computerChoice, playerSelection))
+
+    computerChoice = getComputerChoice(choices);
+
+    playRound(computerChoice, playerSelection);
+    console.log(playRound(computerChoice, playerSelection))
+// logs whether player wins or computer wins via playerCounter or computerCounter
+// ('playerCounter: ' + playerCounter) adds value to string, same with computer counterpart 
+    console.log('playerCounter: ' + playerCounter);
+    console.log('computerCounter: ' + computerCounter);
 
     
+    // this series of if statements defines win/ lose conditions for when all rounds are played
+    // when total computerCounter is more than playerCounter computer wins
+    // when reverse is true player wins 
+    // when counts are equal a tie statement is returned 
+        if (computerCounter > playerCounter) {
+            return 'YOU LOSE THE GAME';
+        }
+
+        if ( computerCounter < playerCounter) { 
+            return 'YOU WIN THE GAME';
+        }
+
+        if (computerCounter == playerCounter) {
+            return 'ITS A TIE! NO ONE WINS( OR MAYBE U BOTH DO )';
+        }
+
+
+ 
+ }
+  
+    
+console.log(game(playRound))
