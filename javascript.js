4f001400ae7d266cd348bf758f592987aa4e5ad2
colin rice <100053207+katrice96@users.noreply.github.com>
2023-10-06 20:54:44 -0400

    refactored game code to prepare for ui removed loop logic replaced loop logic with roundCounter added buttons as a player input removed prompt player input removed game function added playNextRound function this function controls the logic for what happens once round Counter reaches 5 removed redundancies in gameCounter functions logic

--- javascript.js
+++ javascript.js
@@ -1,24 +1,24 @@
+
+let playerSelection;
+
+const rockButton = document.getElementById('rockButton');
+const paperButton = document.getElementById('paperButton');
+const scissorsButton = document.getElementById('scissorsButton');
+
+
 function getComputerChoice(strings) {
-    //randomizes which string the computer returns 
     const randomIndex = Math.floor(Math.random() * strings.length);
-    // tells the computer to return a random string from an array
     return strings[randomIndex]
 }
-// the array of choices the computer pulls from in this case
+
 let choices = ['rock','paper','scissors'];
-// defines the computerChoice variable that tells the getComputerChoice function to pull from the choices array 
 let computerChoice = getComputerChoice(choices);
-//logs result of computerChoice variable in console 
-console.log(computerChoice)
+console.log("Computer Choice: " + computerChoice);
 
 
-// defines playerSelection variable for future use, .prompt brings up a form for the user to input text when page loads.
-// .toLowerCase at end of prompt statement makes sure all inputs are comparable and case insensitive
-
-// defines playRound function which will take computerChoice and playerSelection as parameters and compare outputs of both to determine a winner 
 function playRound(computerChoice, playerSelection) {
    
-   
+   //console.log('playRound');
     //defines what constitutes a tie in the program 
    if (playerSelection === computerChoice) {
     // defines what to output in the case of a tie 
@@ -49,121 +49,77 @@ function playRound(computerChoice, playerSelection) {
     
 // defines both counters 
 // defined outside of a function to allow to iterate each time gameCounter function is called 
-
- let playerCounter = 0;
- let computerCounter = 0;
-    // defines gameCounter function which defines behavior of counters when different conditions are true
- function gameCounter(computerChoice, playerSelection) {
-
-    if (playerSelection === computerChoice) {
+let roundCounter = 0;
+let playerCounter = 0;
+let computerCounter = 0;
+   
  
-        return playerCounter && computerCounter; 
-    
+// defines gameCounter function which defines behavior of counters when different conditions are true
+function gameCounter(computerChoice, playerSelection) {
+    if (playerSelection === computerChoice) { 
         
-       } else if ( 
-        (playerSelection === 'rock' && computerChoice === 'scissors') || 
-        (playerSelection === 'paper' && computerChoice === 'rock') || 
-        (playerSelection ==='scissors' && computerChoice === 'paper')
-    
-       ) {
-        playerCounter += 1; 
-        
-       
-       } else if ( 
-        (playerSelection ==='scissors' && computerChoice === 'rock') || 
-        (playerSelection === 'rock' && computerChoice === 'paper') || 
-        (playerSelection === 'paper' && computerChoice === 'scissors')
-        ) {
-            computerCounter += 1 ;
+        } else if ( 
+            (playerSelection === 'rock' && computerChoice === 'scissors') || 
+            (playerSelection === 'paper' && computerChoice === 'rock') || 
+            (playerSelection ==='scissors' && computerChoice === 'paper')) 
+        {
+            playerCounter += 1; 
+        } else { 
+            computerCounter+= 1;
         }
-        
-        return;
     
- }
-
- // defines the game function which takes in playround function as a parameter to play a five round game of rock paper scissors 
- 
- function game(playRound) { 
-
-    for (let round = 1; round <= 5; round++) {
-
-        //let playerSelection = prompt("this is a game of rock paper scissors, please input your answer").toLowerCase();
-        //console.log(playerSelection);
-        ////////let playerSelection = [rock,paper,scissors]
+}
 
 
-        computerChoice = getComputerChoice(choices);
-        playRound(computerChoice, playerSelection);
-        gameCounter(computerChoice, playerSelection);
-        console.log(playRound(computerChoice, playerSelection))
-        
-        
-    };
+rockButton.addEventListener('click', function() { 
+    playerSelection = 'rock';
+   playRound(computerChoice,playerSelection);
+   gameCounter(computerChoice,playerSelection);
+   roundCounter++
+   playNextRound();
+});
 
-//test function as is w loop, if working correctly then have playerchoice prompt inside of loop 
-// in theory this should allow for user to be prompted for input each time the loop iterates 
-    
+paperButton.addEventListener('click', function() { 
+    playerSelection = 'paper';
+    playRound(computerChoice,playerSelection);
+    gameCounter(computerChoice,playerSelection);
+    roundCounter++
+    playNextRound();
 
-// ('playerCounter: ' + playerCounter) adds value to string, same with computer counterpart 
-    console.log('playerCounter: ' + playerCounter);
-    console.log('computerCounter: ' + computerCounter);
+});
 
-    
-    // this series of if statements defines win/ lose conditions for when all rounds are played
-    // when total computerCounter is more than playerCounter computer wins
-    // when reverse is true player wins 
-    // when counts are equal a tie statement is returned 
-        if (computerCounter > playerCounter) {
-            return 'YOU LOSE THE GAME';
-        }
-
-        if ( computerCounter < playerCounter) { 
-            return 'YOU WIN THE GAME';
-        }
-
-        if (computerCounter == playerCounter) {
-            return 'ITS A TIE! NO ONE WINS( OR MAYBE U BOTH DO )';
-        }
-
-
- 
- }
-  
-    
-console.log(game(playRound))
-
-
- rockButton.addEventListener('click', function() {
-
-    playerSelection = 'Rock';
-    playRound(playerSelection,computerChoice);
-    
-    
- });
-
-
-
-
- paperButton.addEventListener('click', function() {
-
-    playerSelection = 'paper'
-    playRound(playerSelection,computerChoice);
-    
-    
- });
-
-
-
- scissorsButton.addEventListener('click', function() {
-
+scissorsButton.addEventListener('click', function() { 
     playerSelection = 'scissors';
-    playRound(playerSelection,computerChoice);
+    playRound(computerChoice,playerSelection);
+    gameCounter(computerChoice,playerSelection);
+    roundCounter++
+    playNextRound();
     
-    
- });
+});
 
+// defines the game function which takes in playround function as a parameter to play a five round game of rock paper scissors 
  
+function playNextRound() { 
+    if (roundCounter < 5) {
+        computerChoice = getComputerChoice(choices);
+        console.log("Computer Choice: " + computerChoice);
+
+     }else {
+        console.log('playerCounter: ' + playerCounter);
+        console.log('computerCounter: ' + computerCounter);
+
+        if (computerCounter > playerCounter) {
+            console.log('YOU LOSE THE GAME');
+        } else if (computerCounter < playerCounter) {
+            console.log('YOU WIN THE GAME');
+        } else {
+            console.log('IT\'S A TIE! NO ONE WINS (OR MAYBE YOU BOTH DO)');
+        }
+
+    }
 
 
+}
 
+console.log(playNextRound(playRound))
 
